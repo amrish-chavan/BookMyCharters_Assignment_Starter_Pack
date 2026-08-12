84ac8ac51c55e9466e85e97952965a41af4c9a70
amrish-chavan <amrishchavan537@gmail.com>
2026-08-12 11:31:44 +0530

    Remove setup instructions from index.php

--- assignment-1-cache-service/web/index.php
+++ assignment-1-cache-service/web/index.php
@@ -13,21 +13,3 @@ $config = require __DIR__ . '/../config/web.php';
 (new yii\web\Application($config))->run();
 
 
-cd /home/amrish/Documents/PHP/BookMyCharters_Assignment_Starter_Pack/assignment-1-cache-service
-
-# Start MySQL + PHP server (first time builds the Docker image, ~2 min)
-docker compose up -d
-
-# Run migrations
-docker compose exec app php yii migrate --interactive=0
-
-# Run tests
-docker compose exec app php tests/run.php
-
-# Test the API
-curl http://localhost:8080/products/1
-curl http://localhost:8080/categories/1/products
-curl -X PUT http://localhost:8080/products/1 -H "Content-Type: application/json" -d '{"name":"Test","price":100}'
-
-# Stop everything
-docker compose down
